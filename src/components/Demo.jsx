b4747f943494ce39526aecac5aaf16d731aed3cb
Sherry <xinlei1207@gmail.com>
2023-07-14 21:48:20 -0700

    api done

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -84,13 +84,13 @@ const Demo = () => {
               onClick={() => setArticle(item)}
               className='link_card'
             >
-              <div className='copy_btn' onClick={() => handleCopy(item.url)}>
+              {/* <div className='copy_btn' onClick={() => handleCopy(item.url)}>
                 <img
                   src={copied === item.url ? tick : copy}
                   alt={copied === item.url ? "tick_icon" : "copy_icon"}
                   className='w-[40%] h-[40%] object-contain'
                 />
-              </div>
+              </div> */}
               <p className='flex-1 font-satoshi text-blue-700 font-medium text-sm truncate'>
                 {item.url}
               </p>
